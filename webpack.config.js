1a66aba9face327414915f4b6567bd32e4355b9d
Jovana Jovanovska <jovanovska.jovana1@gmail.com>
2018-09-29 02:24:48 +0200

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/script.min.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/style.min.css"
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/view/' },
+      { from: 'src/img/', to: 'img/' }
+    ]),
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8000
+  }
+};
